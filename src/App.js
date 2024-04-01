b5b5db8d7bebe157e1b01534af1199ad0c9254dc
Teslimat <teslimatadeyemi@gmail.com>
2024-04-01 14:22:08 +0100

    made changes to my file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from "react";
 import "./App.css";
-import City from "./City"
 
+import City from "./City";
 function App() {
   return (
     <div className="App">
       <h1>React Debugging from</h1>
-     
-      <City city= "Tokyo"/>
+
+      <City city="Tokyo" />
     </div>
   );
 }
